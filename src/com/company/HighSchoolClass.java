9f4d4859c4872185c2392fa3ae95ebe70b2fc5fb
Dan Pennebacker <dpennebacker@pvsd.org>
2018-11-01 10:17:56 -0400

    Initial commit

---
+++ src/com/company/HighSchoolClass.java
@@ -0,0 +1,23 @@
+package com.company;
+
+public class HighSchoolClass
+{
+    private Student [] students;
+
+    //constructors and other data fields not shown
+    //Show them!
+
+    //returns the valedictorian of the class
+    public Student getValedictorian ()
+    {
+        //to be implemented
+
+    }
+
+    public double getHonorsPercentage ()
+    {
+        //to be implemented
+    }
+
+}
+
---
+++ src/com/company/Student.java
@@ -0,0 +1,26 @@
+package com.company;
+
+public class Student {
+
+    //instance fields go here
+
+    //constructor goes here
+
+
+    // returns the student's grade point average
+    public double getGPA()
+    {
+        //implement here
+    }
+
+    // returns true if the student is in the honors program
+    public boolean isHonors()
+    {
+        //implement here
+    }
+
+
+
+
+
+}
